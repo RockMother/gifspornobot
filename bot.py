ea47e34feb037c2a86371af237471840cbd114a4
Kirill Schumilov <Kirill.Schumilov@t-systems-mms.com>
2017-09-01 17:25:41 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,28 @@
+import reddit
+import telebot
+import os
+from telebot import types
+
+bot = telebot.TeleBot(os.environ['TELEGRAM_TOKEN'])
+
+users = {}
+
+def send_gif(chat_id):
+  gif = reddit.get_random_gif()
+  keyboard = types.InlineKeyboardMarkup()
+  add_more_button = types.InlineKeyboardButton(text="show more", callback_data="more_clicked")
+  keyboard.add(add_more_button)
+  bot.send_message(chat_id, gif, reply_markup=keyboard)
+
+@bot.message_handler(content_types=["text"])
+def message_handler(message):
+  send_gif(message.chat.id)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.message:
+      if call.data == "more_clicked":
+        send_gif(call.message.chat.id)
+
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
---
+++ reddit.py
@@ -0,0 +1,22 @@
+import os
+import praw
+
+def get_link(submission):
+  media = submission.media
+  oembed = media['oembed']
+  return oembed['thumbnail_url']
+
+def get_random_gif():
+  reddit = praw.Reddit(client_id=os.environ['REDDIT_CLIENT_ID'], 
+                        client_secret=os.environ['REDDIT_CLIENT_SECRET'], 
+                        user_agent=os.environ['REDDIT_USER_AGENT'],
+                        username=os.environ['REDDIT_USERNAME'],
+                        password=os.environ['REDDIT_PASSWORD'])
+  subreddit = reddit.subreddit('porn_gifs')   
+  link = None
+  while link == None:
+    try:
+      link = get_link(subreddit.random())
+    except:
+      link = None
+  return link
